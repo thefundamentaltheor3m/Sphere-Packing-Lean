d6ae0d33dc0f5d4dbd450cfdadba09eb3ca5850a
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-03-01 17:38:17 +0100

    Some comments - Sid

--- ProjectCode/Definitions.lean
+++ ProjectCode/Definitions.lean
@@ -8,5 +8,17 @@ open Euclidean
 namespace SpherePacking
 
 def SpherePacking {P : Set V} (hP : Countable P) {r : ℝ} (hr : r > 0)
-  (hPr : ∀ p₁ p₂ : V, p₁ ∈ P → p₂ ∈ P → p₁ ≠ p₂ → Euclidean.dist p₁ p₂ > 2*r) : Set V :=
+  (hPr : ∀ p₁ p₂ : V, p₁ ∈ P → p₂ ∈ P → p₁ ≠ p₂ → Euclidean.dist p₁ p₂ > 2 * r) : Set V :=
   {y | ∃ p : V, p ∈ P ∧ y ∈ ball p r}
+
+-- I will aim to check which of these is a better way of going about it.
+-- Maybe we will also get an idea once we do packing density...
+
+@[ext]
+structure SpherePacking' where
+  centres : Set V
+  hcc : Countable centres -- Hypothesis that centres are countable
+  radius : ℝ
+  hrad : radius > 0 -- Hypothesis that the common radius is positive
+  -- Now, the hypothesis that the spheres form a valid packing
+  hpacking : ∀ p₁ p₂ : V, p₁ ∈ centres → p₂ ∈ centres → p₁ ≠ p₂ → Euclidean.dist p₁ p₂ > 2 * radius
