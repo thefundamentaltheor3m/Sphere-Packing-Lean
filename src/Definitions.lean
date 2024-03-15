0bc4e5c258aeb45ffe2133c2f8ab8584674c4556
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-03-15 14:06:31 +0100

    Attempts with lattice

--- src/Definitions.lean
+++ src/Definitions.lean
@@ -1,15 +1,40 @@
 import Mathlib.Analysis.InnerProductSpace.EuclideanDist
 import Mathlib.Geometry.Euclidean.Sphere.Basic
 import Mathlib.LinearAlgebra.FiniteDimensional
+import Mathlib.LinearAlgebra.GeneralLinearGroup
+import Mathlib.GroupTheory.QuotientGroup
+import Mathlib.LinearAlgebra.Basis
+import Mathlib.LinearAlgebra.Span
+import Mathlib.LinearAlgebra.LinearIndependent
 
 variable (n : ℕ)
 -- The following (copied from Mathlib/MeasureTheory/Integral/TorusIntegral.lean)
 -- allows the local notation ℝⁿ = Fin n → ℝ
 local macro:arg t:term:max noWs "ⁿ" : term => `(Fin n → $t)
 
+instance : AddCommMonoid (ℝⁿ) := Pi.addCommMonoid
+noncomputable instance : Module ℝ (ℝⁿ) := Pi.module (Fin n) (fun i => ℝ) ℝ
+
 open Euclidean
 
-namespace SpherePacking
+section Lattices
+open LinearMap
+
+-- TODO: Definition of Lattice
+
+def elts_of (B : Basis (Fin n) ℝ ℝⁿ) : Set ℝⁿ :=
+  -- ⋃ (i : (Fin n)), {B i}
+  sorry
+
+def lattice' (B : Basis (Fin n) ℝ ℝⁿ) : Set ℝⁿ := -- Submodule.span ℤ (somehow extract elts of B)
+  sorry
+
+def lattice (B : (Fin n) → ℝⁿ) (h : LinearIndependent ℝ B) :
+  Set ℝⁿ :=  sorry
+
+end Lattices
+
+section SpherePacking
 
 /-
 # We define a Sphere Packing as a 5-tuple consisting of
@@ -23,14 +48,16 @@ Remark. We define packings to be extensional: two packings are equal iff they ha
 centres and the same common radius.
 -/
 
+def nonoverlapping (centres : Set ℝⁿ) (radius : ℝ) : Prop :=  ∀ p₁ p₂ : ℝⁿ, p₁ ∈ centres →
+  p₂ ∈ centres → p₁ ≠ p₂ → Euclidean.dist p₁ p₂ > 2 * radius
+
 @[ext]
 structure SpherePacking where
   centres : Set ℝⁿ
   hcc : Countable centres
   radius : ℝ
   hrad : radius > 0
-  hpacking : ∀ p₁ p₂ : ℝⁿ, p₁ ∈ centres → p₂ ∈ centres → p₁ ≠ p₂ →
-    Euclidean.dist p₁ p₂ > 2 * radius
+  hpacking : nonoverlapping n centres radius
 
 #check SpherePacking.ext_iff
 
@@ -52,3 +79,5 @@ def EgPacking1 : SpherePacking 1 where -- An example of a sphere packing in one 
   radius := 1
   hrad := by linarith
   hpacking := sorry
+
+end SpherePacking
