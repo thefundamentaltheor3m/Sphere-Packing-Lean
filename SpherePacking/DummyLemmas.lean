f0d514275805929e86b12d308fa2ca7d120743e5
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-05-28 23:05:35 +0200

    removing all namespaces

--- SpherePacking/DummyLemmas.lean
+++ SpherePacking/DummyLemmas.lean
@@ -1,7 +1,5 @@
 import Mathlib
 
-namespace DummyLemmas
-
 lemma foo1 (n : ℕ) : n = n := by simp
 
 lemma foo2 (n : ℤ) : n = n := sorry
@@ -9,5 +7,3 @@ lemma foo2 (n : ℤ) : n = n := sorry
 lemma foo3 : 1 + 1 = 2 := by simp
 
 lemma foo4 : 3 + 4 = 5 := sorry  -- Let's try a false statement
-
-end DummyLemmas
