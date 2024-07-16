99f9af84e9e5009e055969442c023ab801486c03
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-07-16 13:51:17 +0200

    Minor reorganisation

--- SpherePacking/Basic/E8.lean
+++ SpherePacking/Basic/E8.lean
@@ -13,10 +13,16 @@ instance : HMul ℝ V V := ⟨fun (r : ℝ) (v : V) => (fun i => r * v i)⟩
 def ℤ_as_ℝ : Set ℝ := {r : ℝ | ∃ (n : ℤ), ↑n = r}
 local notation "↑ℤ" => ℤ_as_ℝ
 
+section Definitions
+
 def E8_Set : Set V := {v : V | ((∀ i : Fin 8, v i ∈ ↑ℤ) ∨ (∀ i : Fin 8, (2 * v i) ∈ ↑ℤ
   ∧ (v i ∉ ↑ℤ))) ∧ ∑ i : Fin 8, v i ≡ 0 [PMOD 2]}
 
 def E8_Normalised_Set : Set V := {v : V | ∃ w ∈ E8_Set, v = ((1 : ℝ) / (Real.sqrt 2)) • w}
+
+
+
+end Definitions
 
 noncomputable section Lattice
 
