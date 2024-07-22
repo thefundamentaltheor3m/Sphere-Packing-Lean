1edfe24e13ed02afd0d4b24b2cc2f4ab04b451bc
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-07-22 12:26:15 +0200

    add comment explaining action

--- SpherePacking/Basic/EuclideanLattice.lean
+++ SpherePacking/Basic/EuclideanLattice.lean
@@ -4,13 +4,20 @@ open Euclidean BigOperators
 
 namespace EuclideanLattice  -- Perhaps this can be moved to a different file...
 
-section Definitions
-
 variable {d : ℕ}
 local notation "V" => EuclideanSpace ℝ (Fin d)
 
 class isLattice (Λ : AddSubgroup V) [DiscreteTopology Λ] extends IsZlattice ℝ Λ
 
-end Definitions
+section Action
+
+/-!
+# The Lattice Action
+
+We need to define an action of a lattice on a set of lattice-periodic points in Euclidean space.
+This will allow us to formalise the theorem for the density of a periodic packing.
+-/
+
+end Action
 
 end EuclideanLattice
