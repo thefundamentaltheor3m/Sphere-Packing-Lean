0ed71eebe024cafe3e65101fbda83d5c6f731413
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-08-13 12:22:33 +0200

    add periodic packing stuff to Prereqs

--- SpherePacking/CohnElkies/Prereqs.lean
+++ SpherePacking/CohnElkies/Prereqs.lean
@@ -6,6 +6,7 @@
 import Mathlib.Algebra.Module.Zlattice.Basic
 import Mathlib.Algebra.Module.Zlattice.Covolume
 import Mathlib.Analysis.Fourier.FourierTransform
+import SpherePacking.Basic.SpherePacking
 
 open BigOperators
 
@@ -61,3 +62,47 @@ theorem PSF_L {f : EuclideanSpace ℝ (Fin d) → ℂ} (hf : PSF_Conditions f) :
   sorry
 
 end PSF_L
+
+open scoped ENNReal
+open SpherePacking Metric BigOperators Pointwise Filter MeasureTheory
+
+section Periodic_Packings
+
+/-
+This section consists of two results:
+1. The formula for the density of a periodic packing
+2. The periodic sphere packing constant is the supremum over packings of separation radius 1
+These can be moved to the file `SpherePacking/Basic/PeriodicPacking.lean` being worked on in #25 or
+to the file `SpherePacking/Basic/SpherePacking.lean`.
+-/
+
+theorem periodic_constant_eq_periodic_constant_normalized (hd : 0 < d) :
+    PeriodicSpherePackingConstant d = ⨆ (S : PeriodicSpherePacking d) (_ : S.separation = 1),
+    S.density := by
+  -- Argument almost identical to `constant_eq_constant_normalized`, courtesy Gareth
+  rw [iSup_subtype', PeriodicSpherePackingConstant]
+  apply le_antisymm
+  · apply iSup_le
+    intro S
+    have h := inv_mul_cancel S.separation_pos.ne.symm
+    have := le_iSup (fun x : { x : PeriodicSpherePacking d // x.separation = 1 } ↦ x.val.density)
+        ⟨S.scale (inv_pos.mpr S.separation_pos), h⟩
+    rw [← scale_density hd]
+    · exact this
+    · rw [inv_pos]
+      exact S.separation_pos
+  · apply iSup_le
+    intro ⟨S, _⟩
+    simp only
+    exact le_iSup_iff.mpr fun b a ↦ a S
+
+instance I₁ (S : PeriodicSpherePacking d) : Fintype (Quotient S.instAddAction.orbitRel) := sorry
+
+instance HDiv₁ : HDiv ENNReal ℝ ENNReal := sorry  -- I hope this isn't outright wrong
+
+theorem periodic_density_formula (S : PeriodicSpherePacking d) :
+  S.density = (Fintype.card (Quotient S.instAddAction.orbitRel) : ENNReal) /
+    (Zlattice.covolume S.Λ) * volume (ball (0 : EuclideanSpace ℝ (Fin d)) (S.separation / 2)) := by
+  sorry
+
+end Periodic_Packings
