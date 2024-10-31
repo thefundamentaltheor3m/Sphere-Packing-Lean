13866cb6ee89419cb4a082094562245420bd68bf
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-10-31 13:44:19 +0000

    trying some more defs

--- SpherePacking/MagicFunction/a/Basic.lean
+++ SpherePacking/MagicFunction/a/Basic.lean
@@ -7,7 +7,32 @@ M4R File
 -/
 
 import SpherePacking.ModularForms.Eisenstein
+import Mathlib.Order.Interval.Set.Pi
+import Mathlib.Analysis.Complex.Basic
 
 local notation "V" => EuclideanSpace ℝ (Fin 8)
 
+open Set Complex
 
+noncomputable section Definition
+
+private def z₁ (t : Icc (0 : ℝ) 1) : UpperHalfPlane := ⟨-1 * t + I * (1 - t), by
+
+  sorry⟩
+
+private def z₂ (t : Icc (0 : ℝ) 1) : UpperHalfPlane := ⟨t + I * (1 - t), by
+
+  sorry⟩
+
+private def z₃ (t : Icc (0 : ℝ) 1) : UpperHalfPlane := ⟨I * (1 - t), by
+
+  sorry⟩
+
+private def z₄ (t : NNReal) : UpperHalfPlane := ⟨I * t, by
+
+  sorry⟩
+
+private def I₁ (x : V) := ∫ t in Icc (0 : ℝ) 1, φ₀ ⟨(-1 / (z₁ ⟨t, by
+  sorry⟩ + (1 : ℂ))), sorry⟩ 
+
+end Definition
