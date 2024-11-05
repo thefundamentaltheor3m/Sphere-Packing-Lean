2a29d3ae654de239deb0d0a49ba13fa51c7a4279
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-11-05 19:04:33 +0000

    a defs

--- SpherePacking/MagicFunction/a/Basic.lean
+++ SpherePacking/MagicFunction/a/Basic.lean
@@ -7,21 +7,17 @@ M4R File
 -/
 
 import SpherePacking.ModularForms.Eisenstein
-import Mathlib.Order.Interval.Set.Pi
-import Mathlib.Analysis.Complex.Basic
 import Mathlib
 
 local notation "V" => EuclideanSpace ℝ (Fin 8)
 
-open Set Complex
+open Set Complex Real
 
 noncomputable section Parametrisations
 
 private def z₁ (t : Icc (0 : ℝ) 1) : ℂ := -1 * t + I * (1 - t)
 
 private def z₁' (t : ℝ) : ℂ := IccExtend (zero_le_one) z₁ t -- `by norm_num` also works
-
--- If there's a function that takes x : ℝ to 0 if x ≤ 0, x if x \in
 
 private def z₂ (t : Icc (0 : ℝ) 1) : ℂ := t + I * (1 - t)
 
@@ -41,12 +37,53 @@ section UpperHalfPlane
 
 -- Show that the things that go into φ₀ are in the upper half plane
 
+private lemma z₁_in_upper_half_plane {t : ℝ} (ht : t ∈ Ioo 0 1) : 0 < (z₁' t).im := by
+  have ht' : t ∈ Icc 0 1 := mem_Icc_of_Ioo ht
+  rw [z₁', IccExtend_of_mem zero_le_one z₁ ht', z₁]; simp
+  exact ht.2
+
+private lemma z₂_in_upper_half_plane {t : ℝ} (ht : t ∈ Ioo 0 1) : 0 < (z₂' t).im := by
+  have ht' : t ∈ Icc 0 1 := mem_Icc_of_Ioo ht
+  rw [z₂', IccExtend_of_mem zero_le_one z₂ ht', z₂]; simp
+  exact ht.2
+
+private lemma z₃_in_upper_half_plane {t : ℝ} (ht : t ∈ Ioo 0 1) : 0 < (z₃' t).im := by
+  have ht' : t ∈ Icc 0 1 := mem_Icc_of_Ioo ht
+  rw [z₃', IccExtend_of_mem zero_le_one z₃ ht', z₃]; simp
+  exact ht.2
+
+private lemma z₄_in_upper_half_plane {t : ℝ} (ht : t ∈ Ioi 0) : 0 < (z₄' t).im := by
+  have ht' : t ∈ Ici 0 := mem_Ici_of_Ioi ht
+  rw [z₄', IciExtend_of_mem z₄ ht', z₄]; simp
+  exact ht
+
 end UpperHalfPlane
 
 noncomputable section Integrals
 
-private noncomputable def I₁ (x : V) := ∫ t in Icc (0 : ℝ) 1, φ₀'' (-1 / ((z₁' t) + (1 : ℂ)))
+private def I₁ (x : V) := ∫ t in Ioo (0 : ℝ) 1,
+  φ₀'' (-1 / ((z₁' t) + (1 : ℂ))) *
+  ((z₁' t) + (1 : ℂ)) ^ 2 *
+  cexp (π * I * ‖x‖ ^ 2 * (z₁' t))
+
+private def I₂ (x : V) := ∫ t in Ioo (0 : ℝ) 1,
+  φ₀'' (-1 / ((z₂' t) - (1 : ℂ))) *
+  ((z₂' t) - (1 : ℂ)) ^ 2 *
+  cexp (π * I * ‖x‖ ^ 2 * (z₂' t))
+
+private def I₃ (x : V) := ∫ t in Ioo (0 : ℝ) 1,
+  φ₀'' (-1 / (z₃' t)) *
+  (z₃' t) ^ 2 *
+  cexp (π * I * ‖x‖ ^ 2 * (z₃' t))
+
+private def I₄ (x : V) := ∫ t in Ioi (0 : ℝ),
+  φ₀'' (-1 / z₄' t) *
+  (z₄' t) ^ 2 *
+  cexp (π * I * ‖x‖ ^ 2 * (z₄' t))
 
 end Integrals
+
+noncomputable def a (x : V) := I₁ x + I₂ x + I₃ x + I₄ x
+#check a
 
 #min_imports
--- SpherePacking/ModularForms/Eisenstein.lean
+++ SpherePacking/ModularForms/Eisenstein.lean
@@ -7,24 +7,40 @@ import Mathlib.NumberTheory.ModularForms.EisensteinSeries.Basic
 
 open EisensteinSeries ModularForm
 
+noncomputable section Definitions
+
 def standardcongruencecondition : Fin 2 → ZMod ((1 : ℕ+) : ℕ) := 0
 
 private lemma aux4 : (3 : ℤ) ≤ 4 := by norm_num
 private lemma aux6 : (3 : ℤ) ≤ 6 := by norm_num
 
-noncomputable def E₂ := eisensteinSeries_SIF standardcongruencecondition 2
-noncomputable def E₄ := eisensteinSeries_MF aux4 standardcongruencecondition
-noncomputable def E₆ := eisensteinSeries_MF aux6 standardcongruencecondition
-
--- Need to try and get holomorphicity conditions too
+/- The Eisenstein Series E₂, E₄ and E₆ -/
+def E₂ := eisensteinSeries_SIF standardcongruencecondition 2
+def E₄ := eisensteinSeries_MF aux4 standardcongruencecondition
+def E₆ := eisensteinSeries_MF aux6 standardcongruencecondition
 
 /- The discriminant form -/
-noncomputable def Δ (z : UpperHalfPlane) := ((E₄ z) ^ 3 - (E₆ z) ^ 2) / 1728
+def Δ (z : UpperHalfPlane) := ((E₄ z) ^ 3 - (E₆ z) ^ 2) / 1728
 
 /- φ₀, φ₋₂ and φ₋₄, except we can't use - signs in subscripts for definitions... -/
-noncomputable def φ₀ (z : UpperHalfPlane) := (((E₂ z) * (E₄ z) - (E₆ z)) ^ 2) / (Δ z)
-noncomputable def φ₂' (z : UpperHalfPlane) := (E₄ z) * ((E₂ z) * (E₄ z) - (E₆ z)) / (Δ z)
-noncomputable def φ₄' (z : UpperHalfPlane) := ((E₄ z) ^ 2) / (Δ z)
+def φ₀ (z : UpperHalfPlane) := (((E₂ z) * (E₄ z) - (E₆ z)) ^ 2) / (Δ z)
+def φ₂' (z : UpperHalfPlane) := (E₄ z) * ((E₂ z) * (E₄ z) - (E₆ z)) / (Δ z)
+def φ₄' (z : UpperHalfPlane) := ((E₄ z) ^ 2) / (Δ z)
+/- We extend these definitions to ℂ for convenience. -/
+def φ₀'' (z : ℂ) : ℂ := if hz : 0 < z.im then φ₀ ⟨z, hz⟩ else 0
+def φ₂'' (z : ℂ) : ℂ := if hz : 0 < z.im then φ₂' ⟨z, hz⟩ else 0
+def φ₄'' (z : ℂ) : ℂ := if hz : 0 < z.im then φ₄' ⟨z, hz⟩ else 0
 
-noncomputable def φ₀'' (z : ℂ) : ℂ :=
-  if hz : 0 < z.im then φ₀ ⟨z, hz⟩ else 0
+end Definitions
+
+noncomputable section Holomorphicity
+
+-- Try and get the desired holomorphicity results for φ₀, φ₂ and φ₄ in terms of the Eᵢ
+
+end Holomorphicity
+
+noncomputable section Integrability
+
+-- Here, we show that 
+
+end Integrability
