18b051a9c0e634be8df5b65d2baa516d71f78f5c
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2024-11-20 19:49:01 +0000

    Big ugly fix for GL(n, ℝ)⁺ om ForMathlib/SlashActionAuxil.lean

--- SpherePacking/ForMathlib/SlashActions.lean
+++ SpherePacking/ForMathlib/SlashActions.lean
@@ -7,35 +7,43 @@ local notation "GL(" n ", " R ")" "⁺" => @Matrix.GLPos (Fin n) R (instDecidabl
 
 open ModularForm MatrixGroups UpperHalfPlane
 
-/- Looks like the way to fix the errors is to replace each GL(2, ℝ)⁺ with
+/- Looks like the way to fix the errors is to replace each GL(n, ℝ)⁺ with
   `@Matrix.GLPos (Fin 2) ℝ (instDecidableEqFin 2) (Fin.fintype 2) Real.linearOrderedCommRing :
   Subgroup (GL (Fin 2) ℝ)`... but it's just so ugly!
 -/
 
-/-- Slash action under -I₂ as a GL(2, ℝ)⁺ matrix. See `ModularForm.slash_neg_one'` for the SL(2, ℤ)
+/-- Slash action under -I₂ as a GL(n, ℝ)⁺ matrix. See `ModularForm.slash_neg_one'` for the SL(2, ℤ)
 version. -/
 theorem ModularForm.slash_neg_one {k : ℤ} (f : ℍ → ℂ) (hk : Even k) :
-    f ∣[k] (-1 : GL(2, ℝ)⁺) = f ∣[k] (1 : GL(2, ℝ)⁺) := by
+    f ∣[k] (-1 : (@Matrix.GLPos (Fin 2) ℝ (instDecidableEqFin 2) (Fin.fintype 2)
+    Real.linearOrderedCommRing : Subgroup (GL (Fin 2) ℝ))) =
+    f ∣[k] (1 : (@Matrix.GLPos (Fin 2) ℝ (instDecidableEqFin 2) (Fin.fintype 2)
+    Real.linearOrderedCommRing :
+  Subgroup (GL (Fin 2) ℝ))) := by
   ext x
   simp [slash_def, slash, denom, hk.neg_one_zpow, Matrix.det_neg]
 
-/-- Slash action under -I₂ as a SL(2, ℤ) matrix. See `ModularForm.slash_neg_one` for the GL(2, ℝ)⁺
+/-- Slash action under -I₂ as a SL(2, ℤ) matrix. See `ModularForm.slash_neg_one` for the GL(n, ℝ)⁺
 version. -/
 theorem ModularForm.slash_neg_one' {k : ℤ} (f : ℍ → ℂ) (hk : Even k) :
     f ∣[k] (-1 : SL(2, ℤ)) = f ∣[k] (1 : SL(2, ℤ)) := by
   ext x
-  have : ((-1 : SL(2, ℤ)) : GL(2, ℝ)⁺) • x = x := by ext; simp [num, denom]
+  have : ((-1 : SL(2, ℤ)) : (@Matrix.GLPos (Fin 2) ℝ (instDecidableEqFin 2)
+  (Fin.fintype 2) Real.linearOrderedCommRing :
+  Subgroup (GL (Fin 2) ℝ))) • x = x := by ext; simp [num, denom]
   simp [slash_def, slash, denom, hk.neg_one_zpow, this]
 
 /-- See `ModularForm.slash_neg'` for the version where `g` is a SL(2, ℤ) matrix. -/
-theorem ModularForm.slash_neg {k : ℤ} (g : GL(2, ℝ)⁺) (f : ℍ → ℂ) (hk : Even k) :
+theorem ModularForm.slash_neg {k : ℤ} (g : (@Matrix.GLPos (Fin 2) ℝ (instDecidableEqFin 2)
+(Fin.fintype 2) Real.linearOrderedCommRing :
+  Subgroup (GL (Fin 2) ℝ))) (f : ℍ → ℂ) (hk : Even k) :
     f ∣[k] (-g) = f ∣[k] g := by
   rw [← neg_one_mul, SlashAction.slash_mul, slash_neg_one f hk, SlashAction.slash_one]
 
-/-- See `ModularForm.slash_neg` for the version where `g` is a GL(2, ℝ)⁺ matrix. -/
+/-- See `ModularForm.slash_neg` for the version where `g` is a GL(n, ℝ)⁺ matrix. -/
 theorem ModularForm.slash_neg' {k : ℤ} (g : SL(2, ℤ)) (f : ℍ → ℂ) (hk : Even k) :
     f ∣[k] (-g) = f ∣[k] g := by
   rw [SL_slash, ← slash_neg _ _ hk]
   congr
   ext
-  simp [ModularGroup.coe']
+  simp [ModularGroup.coe]
