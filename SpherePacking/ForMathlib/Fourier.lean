3ffebbe0632a688ac756545491e441fab307ae19
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2025-02-09 16:16:41 +0000

    modify hasFourierSeries results with `extend`

--- SpherePacking/ForMathlib/Fourier.lean
+++ SpherePacking/ForMathlib/Fourier.lean
@@ -15,34 +15,47 @@ open Complex Real
 variable {U : Set ℂ} (f : U → ℂ) (c : ℕ → ℂ)
 
 def HasFourierSeries : Prop := ∀ (x : U),
-  Summable (fun n ↦ c n * exp (π * I * n * x)) ∧ f x = ∑' n, c n * exp (n * I * n * x)
+  HasSum (fun n ↦ c n * exp (π * I * n * x)) (f x)
 
 def Has2PiFourierSeries : Prop := ∀ (x : U),
-  Summable (fun n ↦ c n * exp (2 * π * I * n * x)) ∧ f x = ∑' n, c n * exp (2 * π * I * n * x)
+  HasSum (fun n ↦ c n * exp (2 * π * I * n * x)) (f x)
 
 section Odd_Even_Trick
 
-theorem HasFourierSeries.has2PiFourierSeries (H : HasFourierSeries f c) :
-    Has2PiFourierSeries f (fun n ↦ (if Odd n then 0 else c (n / 2))) := by
-  rw [HasFourierSeries] at H
-  rw [Has2PiFourierSeries]
-  intro x
-  obtain ⟨H₁, H₂⟩ := H x
-  simp
+open Function
+
+theorem hasFourierSeries_iff_has2PiFourierSeries :
+  HasFourierSeries f c ↔ Has2PiFourierSeries f (extend (fun n ↦ 2 * n) c 0) := by
+  simp [HasFourierSeries, Has2PiFourierSeries]
+  have hinj : Injective (fun n ↦ 2 * n) := Ring.nsmul_right_injective (by positivity)
   constructor
-  ·
-    stop
-    obtain ⟨L, hL⟩ := H₁
-    use L
-    intro ε hε
-    specialize hL hε
-    simp at hL ⊢
-    obtain ⟨a, ha⟩ := hL
-    use a
-    intro b hb
-    specialize ha b hb
+  · intro H x hx
+    specialize H x hx
+    
     sorry
-  · rw [H₂]
-    sorry
+  sorry
+
+-- theorem HasFourierSeries.has2PiFourierSeries (H : HasFourierSeries f c) :
+--     Has2PiFourierSeries f (fun n ↦ (if Odd n then 0 else c (n / 2))) := by
+--   rw [HasFourierSeries] at H
+--   rw [Has2PiFourierSeries]
+--   intro x
+--   obtain ⟨H₁, H₂⟩ := H x
+--   simp
+--   constructor
+--   ·
+--     -- stop
+--     obtain ⟨L, hL⟩ := H₁
+--     use L
+--     intro ε hε
+--     specialize hL hε
+--     simp at hL ⊢
+--     obtain ⟨a, ha⟩ := hL
+--     use Finset.image (fun n => 2 * n) a
+--     intro b hb
+--     rw [Finset.sum_ite]
+--     sorry
+--   · rw [H₂]
+--     sorry
 
 end Odd_Even_Trick
