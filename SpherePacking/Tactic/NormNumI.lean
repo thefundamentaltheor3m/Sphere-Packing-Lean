52cf2482ce8e36c255037738f177e834db3620c9
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2025-06-12 14:06:46 +0100

    trying to make `norm_numI` its own tactic

--- SpherePacking/Tactic/NormNumI.lean
+++ SpherePacking/Tactic/NormNumI.lean
@@ -3,78 +3,85 @@ Copyright (c) 2025 Heather Macbeth. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth, Yunzhou Xie, Sidharth Hariharan
 -/
+import Mathlib.Tactic.NormNum
+
 import Mathlib.Data.Complex.Basic
 
 open Lean Meta Elab Qq Tactic Complex Mathlib.Tactic
 open ComplexConjugate
 
+example : 1 + 1 = 2 ∧ 2 + 1 = 3 := by norm_num
+
 namespace Mathlib.Meta
 namespace NormNumI
 
-theorem split_I : I = ⟨0, 1⟩ := rfl
+theorem split_I : I = ⟨(0 : ℚ), (1 : ℚ)⟩ := by norm_cast
 
-theorem split_zero : (0 : ℂ) = ⟨0, 0⟩ := rfl
+theorem split_zero : (0 : ℂ) = ⟨(0 : ℚ), (0 : ℚ)⟩ := by norm_cast
 
-theorem split_one : (1 : ℂ) = ⟨1, 0⟩ := rfl
+theorem split_one : (1 : ℂ) = ⟨(1 : ℚ), (0 : ℚ)⟩ := by norm_cast
 
-theorem split_add {z₁ z₂ : ℂ} {a₁ a₂ b₁ b₂ : ℝ}
+theorem split_add {z₁ z₂ : ℂ} {a₁ a₂ b₁ b₂ : ℚ}
     (h₁ : z₁ = ⟨a₁, b₁⟩) (h₂ : z₂ = ⟨a₂, b₂⟩) :
-    z₁ + z₂ = ⟨(a₁ + a₂), (b₁ + b₂)⟩ := by
+    z₁ + z₂ = ⟨(a₁ + a₂ : ℚ), (b₁ + b₂ : ℚ)⟩ := by
   substs h₁ h₂
-  rfl
+  simp [Complex.ext_iff]
 
-theorem split_mul {z₁ z₂ : ℂ} {a₁ a₂ b₁ b₂ : ℝ} (h₁ : z₁ = ⟨a₁, b₁⟩) (h₂ : z₂ = ⟨a₂, b₂⟩) :
-    z₁ * z₂ = ⟨(a₁ * a₂ - b₁ * b₂), (a₁ * b₂ + b₁ * a₂)⟩ :=
-  Ring.mul_congr h₁ h₂ rfl
+theorem split_mul {z₁ z₂ : ℂ} {a₁ a₂ b₁ b₂ : ℚ} (h₁ : z₁ = ⟨a₁, b₁⟩) (h₂ : z₂ = ⟨a₂, b₂⟩) :
+    z₁ * z₂ = ⟨(a₁ * a₂ - b₁ * b₂ : ℚ), (a₁ * b₂ + b₁ * a₂ : ℚ)⟩ :=
+  Ring.mul_congr h₁ h₂ (by simp [Complex.ext_iff])
 
-theorem split_inv {z : ℂ} {x y : ℝ} (h : z = ⟨x, y⟩) :
-    z⁻¹ = ⟨x / (x * x + y * y), - y / (x * x + y * y)⟩ := by
+theorem split_inv {z : ℂ} {x y : ℚ} (h : z = ⟨x, y⟩) :
+    z⁻¹ = ⟨(x / (x * x + y * y) : ℚ), (- y / (x * x + y * y) : ℚ)⟩ := by
   subst h
   rw [inv_def]
   exact Complex.ext (by simp [normSq_apply]; rfl) (by simp [normSq_apply, neg_div]; rfl)
 
-theorem split_neg {z : ℂ} {a b : ℝ} (h : z = ⟨a, b⟩) :
-    -z = ⟨-a, -b⟩ := by
+theorem split_neg {z : ℂ} {a b : ℚ} (h : z = ⟨a, b⟩) :
+    -z = ⟨(-a : ℚ), (-b : ℚ)⟩ := by
   subst h
-  rfl
+  simp [Complex.ext_iff]
 
-theorem split_conj {w : ℂ} {a b : ℝ} (hw : w = ⟨a, b⟩):
-    conj w = ⟨a, -b⟩ := by
+theorem split_conj {w : ℂ} {a b : ℚ} (hw : w = ⟨a, b⟩):
+    conj w = ⟨a, (-b : ℚ)⟩ := by
   rw [hw]
-  rfl
+  simp [Complex.ext_iff]
 
 theorem split_num (n : ℕ) [n.AtLeastTwo]:
-    OfNat.ofNat (α := ℂ) n = ⟨OfNat.ofNat n, 0⟩ := rfl
+    OfNat.ofNat (α := ℂ) n = ⟨((OfNat.ofNat n) : ℚ), (0 : ℚ)⟩ := by norm_cast
 
 theorem split_scientific (m exp : ℕ) (x : Bool) :
-    (OfScientific.ofScientific m x exp : ℂ) = ⟨(OfScientific.ofScientific m x exp : ℝ), 0⟩ :=
-  rfl
+    (OfScientific.ofScientific m x exp : ℂ) = ⟨(OfScientific.ofScientific m x exp : ℚ), (0 : ℚ)⟩ :=
+  by norm_cast
 
-theorem eq_eq {z : ℂ} {a b a' b' : ℝ} (pf : z = ⟨a, b⟩)
+theorem eq_eq {z : ℂ} {a b a' b' : ℚ} (pf : z = ⟨a, b⟩)
   (pf_a : a = a') (pf_b : b = b') :
   z = ⟨a', b'⟩ := by simp_all
 
-theorem eq_of_eq_of_eq_of_eq {z w : ℂ} {az bz aw bw : ℝ} (hz : z = ⟨az, bz⟩) (hw : w = ⟨aw, bw⟩)
+theorem eq_of_eq_of_eq_of_eq {z w : ℂ} {az bz aw bw : ℚ} (hz : z = ⟨az, bz⟩) (hw : w = ⟨aw, bw⟩)
     (ha : az = aw) (hb : bz = bw) : z = w := by
   simp [hz, hw, ha, hb]
 
-theorem ne_of_re_ne {z w : ℂ} {az bz aw bw : ℝ} (hz : z = ⟨az, bz⟩) (hw : w = ⟨aw, bw⟩)
+theorem eq_of_eq_of_eq_and_eq {z w : ℂ} {az bz aw bw : ℚ} (hz : z = ⟨az, bz⟩) (hw : w = ⟨aw, bw⟩)
+    (h : az = aw ∧ bz = bw) : z = w := eq_of_eq_of_eq_of_eq hz hw h.1 h.2
+
+theorem neq_of_re_neq {z w : ℂ} {az bz aw bw : ℚ} (hz : z = ⟨az, bz⟩) (hw : w = ⟨aw, bw⟩)
     (ha : az ≠ aw) : z ≠ w := by
   simp [hz, hw, ha]
 
-theorem ne_of_im_ne {z w : ℂ} {az bz aw bw : ℝ} (hz : z = ⟨az, bz⟩) (hw : w = ⟨aw, bw⟩)
+theorem neq_of_im_neq {z w : ℂ} {az bz aw bw : ℚ} (hz : z = ⟨az, bz⟩) (hw : w = ⟨aw, bw⟩)
     (hb : bz ≠ bw) : z ≠ w := by
   simp [hz, hw, hb]
 
-theorem re_eq_of_eq {z : ℂ} {a b : ℝ} (hz : z = ⟨a, b⟩) : Complex.re z = a := by
+theorem re_eq_of_eq {z : ℂ} {a b : ℚ} (hz : z = ⟨a, b⟩) : Complex.re z = a := by
   simp [hz]
 
-theorem im_eq_of_eq {z : ℂ} {a b : ℝ} (hz : z = ⟨a, b⟩) : Complex.im z = b := by
+theorem im_eq_of_eq {z : ℂ} {a b : ℚ} (hz : z = ⟨a, b⟩) : Complex.im z = b := by
   simp [hz]
 
 set_option pp.showLetValues true
 partial def parse (z : Q(ℂ)) :
-    MetaM (Σ a b : Q(ℝ),  Q($z = ⟨$a, $b⟩)) := do
+    MetaM (Σ a b : Q(ℚ),  Q($z = ⟨$a, $b⟩)) := do
   match z with
   /- parse an addition: `z₁ + z₂` -/
   | ~q($z₁ + $z₂) =>
@@ -110,7 +117,7 @@ partial def parse (z : Q(ℂ)) :
     match n.natLit! with
     | 0 =>
       let _i : $n =Q 0 := ⟨⟩
-      return ⟨q(1), q(0), q(show $w ^ $n' = _ from $(hn).out ▸ pow_zero _)⟩
+      return ⟨q(1), q(0), q(show $w ^ $n' = _ from $(hn).out ▸ sorry)⟩
     | n + 1 =>
       parse q($w^$n * $w)
   /- parse integer power -/
@@ -150,12 +157,12 @@ partial def parse (z : Q(ℂ)) :
   --   pure ⟨a, b, q(rfl)⟩
   | _ => throwError "found the atom {z} which is not a numeral"
 
-def normalize (z : Q(ℂ)) : MetaM (Σ a b : Q(ℝ), Q($z = ⟨$a, $b⟩)) := do
+def normalize (z : Q(ℂ)) : MetaM (Σ a b : Q(ℚ), Q($z = ⟨$a, $b⟩)) := do
   let ⟨a, b, pf⟩ ← parse z
-  let ra ← Mathlib.Meta.NormNum.derive (α := q(ℝ)) a
-  let rb ← Mathlib.Meta.NormNum.derive (α := q(ℝ)) b
-  let { expr := (a' : Q(ℝ)), proof? := (pf_a : Q($a = $a')) } ← ra.toSimpResult | unreachable!
-  let { expr := (b' : Q(ℝ)), proof? := (pf_b : Q($b = $b')) } ← rb.toSimpResult | unreachable!
+  let ra ← Mathlib.Meta.NormNum.derive (α := q(ℚ)) a
+  let rb ← Mathlib.Meta.NormNum.derive (α := q(ℚ)) b
+  let { expr := (a' : Q(ℚ)), proof? := (pf_a : Q($a = $a')) } ← ra.toSimpResult | unreachable!
+  let { expr := (b' : Q(ℚ)), proof? := (pf_b : Q($b = $b')) } ← rb.toSimpResult | unreachable!
   return ⟨a', b', q(eq_eq $pf $pf_a $pf_b)⟩
 
 elab "norm_numI" : conv => do
@@ -165,6 +172,88 @@ elab "norm_numI" : conv => do
   let ⟨a, b, pf⟩ ← normalize z
   Conv.applySimpResult { expr := q(Complex.mk $a $b), proof? := some pf }
 
+elab "norm_numI" : tactic => liftMetaFinishingTactic fun g ↦ do
+  let tgt ← g.getType
+  if let some (_, a, b) := tgt.eq? then
+    let ⟨0, A, a⟩ ← inferTypeQ' a |
+      throwError "norm_num tactic only works for complex numbers"
+    let .defEq h := ← isDefEqQ q($A) q(ℂ) |
+      throwError "norm_num tactic only works for complex numbers, not {A}"
+    have b : Q(ℂ) := b
+    let ⟨x₁, y₁, pf_a⟩ ← normalize a
+    let ⟨x₂, y₂, pf_b⟩ ← normalize b
+    -- Lean.logInfo "Proving eq"
+    let ⟨iseqx, eqx⟩ ← NormNum.deriveBool q($x₁ = $x₂ ∧ $y₁ = $y₂)
+    match iseqx with
+    | true =>
+      -- Lean.logInfo "Proved eq"
+      let pf : Q($a = $b) := q(eq_of_eq_of_eq_and_eq $pf_a $pf_b $eqx)
+      g.assign pf
+    | _ =>
+      throwError "LHS normalizes to {q(Complex.mk $x₁ $y₁)}, RHS normalizes to {q(Complex.mk $x₂ $y₂)}"
+    -- if (x₁ == x₂) ∧ (y₁ == y₂) then
+    --   let _ : $x₁ =Q $x₂ := ⟨⟩
+    --   let _ : $y₁ =Q $y₂ := ⟨⟩
+    --   let pf : Q($a = $b) := q(eq_of_eq_of_eq_of_eq $pf_a $pf_b rfl rfl)
+    --   g.assign pf
+    -- else
+  -- else if let some (_, a, b) := tgt.ne? then
+  --   let ⟨_, A, a⟩ ← inferTypeQ' a
+  --   unless (← isDefEq A q(ℂ)) do
+  --     throwError "norm_num tactic only works for complex numbers, not {A}"
+  --   have a : Q(ℂ) := a
+  --   have b : Q(ℂ) := b
+  --   let ⟨x₁, y₁, pf_a⟩ ← normalize a
+  --   let ⟨x₂, y₂, pf_b⟩ ← normalize b
+  --   if heq : (x₁ == x₂) ∧ (y₁ == y₂) then
+  --     throwError "LHS normalizes to {q(Complex.mk $x₁ $y₁)}, RHS normalizes to {q(Complex.mk $x₂ $y₂)}"
+  --   else if hne : (x₁ != x₂) ∧ (y₁ == y₂) then
+  --     let _ : ¬($x₁ =Q $x₂) := fun h ↦ by absurd hne.1; sorry
+  --     -- have pf_re : x₁ ≠ x₂ := by
+  --     --   rw [← bne_iff_ne]
+  --     --   sorry
+  --     -- We know x₁ ≠ x₂, so we can use this directly
+  --     let pf : Q($a ≠ $b) := q(neq_of_re_neq $pf_a $pf_b sorry)
+  --     g.assign pf
+  --   else
+  --     have pf_im : Q($y₁ ≠ $y₂) := tgt
+  --     let pf : Q($a ≠ $b) := q(neq_of_im_neq $pf_a $pf_b $pf_im)
+  --     g.assign pf
+  else if let some (_, a, b) := tgt.ne? then
+    let ⟨0, A, a⟩ ← inferTypeQ' a |
+      throwError "norm_num tactic only works for complex numbers"
+    let .defEq h := ← isDefEqQ q($A) q(ℂ) |
+      throwError "norm_num tactic only works for complex numbers, not {A}"
+    have b : Q(ℂ) := b
+    let ⟨x₁, y₁, pf_a⟩ ← normalize a
+    let ⟨x₂, y₂, pf_b⟩ ← normalize b
+    -- Lean.logInfo "Proving eq"
+    let ⟨iseqx, eqx⟩ ← NormNum.deriveBool q($x₁ = $x₂ ∧ $y₁ = $y₂)
+    match iseqx with
+    | true =>
+      -- Lean.logInfo "Proved eq"
+      let pf : Q($a = $b) := q(eq_of_eq_of_eq_and_eq $pf_a $pf_b $eqx)
+      g.assign pf
+    | _ =>
+      throwError "LHS normalizes to {q(Complex.mk $x₁ $y₁)}, RHS normalizes to {q(Complex.mk $x₂ $y₂)}"
+  else throwError "not `=` or `≠` goal"
+  -- else if let some (α, a, b) := tgt.ne? then
+  --   let ⟨u, A, a⟩ ← inferTypeQ' a
+  --   unless (← isDefEq A q(ℂ)) do
+  --     throwError "norm_num tactic only works for complex numbers, not {A}"
+  --   have a : Q(ℂ) := a
+  --   have b : Q(ℂ) := b
+  --   let ⟨x₁, y₁, pf_a⟩ ← normalize a
+  --   let ⟨x₂, y₂, pf_b⟩ ← normalize b
+  --   throwError "LHS normalizes to {q(Complex.mk $x₁ $x₁)}, RHS normalizes to {q(Complex.mk $x₂ $y₂)}"
+  -- else
+  --   throwError "norm_num tactic only handles = or ≠ goals, not {tgt}!"
+
+-- #conv norm_numI => (1.5 : ℂ)
+
+-- set_option trace.debug true
+
+#check norm_num
 -- -- Testing the `parse` function
 -- elab "norm_numI_parse" : conv => do
 --   let z ← Conv.getLhs
@@ -173,7 +262,21 @@ elab "norm_numI" : conv => do
 --   let ⟨a, b, pf⟩ ← parse z
 --   Conv.applySimpResult { expr := q(Complex.mk $a $b), proof? := some pf }
 
+example : (1 + 3.5 + I) * (1 + I) = 7 / 2 + 11 / 2 * I := by norm_numI
+example : (3 + 4 * I)⁻¹ * (3 + 4 * I) = 1 := by norm_numI
+example : -1 / (1 + I) = (I - 1) / 2 := by norm_numI
+example : (1 + I) * (1 - I) = 2 := by norm_numI
+example : (1 + 2 * I) - (1 + 2 * I) = 0 := by norm_numI
+example : (conj (3 + 4 * I) : ℂ) * (3 + 4 * I) = 25 := by norm_numI
+example : (3 + I : ℂ) ^ 2 = 8 + 6 * I := by norm_numI
+example : (3 : ℂ) ^ 2 + (4 : ℂ) ^ 2 = 25 := by norm_numI
+
+-- example : 2 = 3 := by norm_num
+
 end NormNumI
+
+-- #exit
+
 namespace NormNum
 
 /-- The `norm_num` extension which identifies expressions of the form `(z : ℂ) = (w : ℂ)`,
@@ -196,14 +299,14 @@ such that `norm_num` successfully recognises both the real and imaginary parts o
     let ⟨bb, rb⟩ ← deriveBool q($bz = $bw)
     match bb with
     | true => return Result'.isBool true q(NormNumI.eq_of_eq_of_eq_of_eq $pfz $pfw $ra $rb)
-    | false => return Result'.isBool false q(NormNumI.ne_of_im_ne $pfz $pfw $rb)
-  | false => return Result'.isBool false q(NormNumI.ne_of_re_ne $pfz $pfw $ra)
+    | false => return Result'.isBool false q(NormNumI.neq_of_im_neq $pfz $pfw $rb)
+  | false => return Result'.isBool false q(NormNumI.neq_of_re_neq $pfz $pfw $ra)
 
 /-- The `norm_num` extension which identifies expressions of the form `Complex.re (z : ℂ)`,
 such that `norm_num` successfully recognises the real part of `z`.
 -/
 @[norm_num Complex.re _] def evalRe : NormNumExt where eval {v β} e := do
-  haveI' : v =QL 0 := ⟨⟩; haveI' : $β =Q ℝ := ⟨⟩
+  haveI' : v =QL 0 := ⟨⟩; haveI' : $β =Q ℚ := ⟨⟩
   let .proj ``Complex 0 z ← whnfR e | failure
   have z : Q(ℂ) := z
   haveI' : $e =Q (Complex.re $z) := ⟨⟩
@@ -215,7 +318,7 @@ such that `norm_num` successfully recognises the real part of `z`.
 such that `norm_num` successfully recognises the imaginary part of `z`.
 -/
 @[norm_num Complex.im _] def evalIm : NormNumExt where eval {v β} e := do
-  haveI' : v =QL 0 := ⟨⟩; haveI' : $β =Q ℝ := ⟨⟩
+  haveI' : v =QL 0 := ⟨⟩; haveI' : $β =Q ℚ := ⟨⟩
   let .proj ``Complex 1 z ← whnfR e | failure
   have z : Q(ℂ) := z
   haveI' : $e =Q (Complex.im $z) := ⟨⟩
--- SpherePacking/Tactic/Test/NormNumI.lean
+++ SpherePacking/Tactic/Test/NormNumI.lean
@@ -20,19 +20,21 @@ example : (3 + 4.5 * I)⁻¹ * (3 + 4.5 * I) = 1 := by norm_num1
 example : -1 / (1 + I) = (I - 1) / 2 := by norm_num1
 example : (I:ℂ) = 0 + 1 * I := by norm_num1
 example : (1.5:ℂ) = ⟨3 / 2, 0⟩ := by conv_lhs => norm_numI
-example : 0 + (1:ℂ) = 1 := by norm_num1
-example : (1.0:ℂ) + 0 = 1 := by norm_num1
-example : (1.0:ℂ) + 0.5 = 3/2 := by norm_num1
-example : I + (3/2:ℂ) = 3/2 + I := by norm_num1
+example : 0 + (1:ℂ) = 1 := by norm_numI
+example : (1.0:ℂ) + 0 = 1 := by norm_numI
+example : (1.0:ℂ) + 0.5 = 3/2 := by norm_numI
+example : I + (3/2:ℂ) = 3/2 + I := by norm_numI
 
 example : I + (3/2:ℂ) = 3/2 + I := by
   conv_lhs => norm_numI
   conv_rhs => norm_numI
+  conv_lhs => norm_numI
+  conv_rhs => norm_numI
 
-example : 2 * (2.5:ℂ) = 5 := by norm_num1
+example : 2 * (2.5:ℂ) = 5 := by norm_numI
 
 -- Playing with the `parse` function
-example : (1 + I) * (1 + I * I * I) = 2 := by norm_num1
+example : (1 + I) * (1 + I * I * I) = 2 := by norm_numI
 -- #conv norm_numI_parse => ((1 + I) * (1 + I * I * I))
 -- #conv norm_numI_parse => (2 : ℂ)
 -- #conv norm_numI => ((1 + I) * (1 + I * I * I))
@@ -40,7 +42,7 @@ example : (1 + I) * (1 + I * I * I) = 2 := by norm_num1
 example : (1 + I) * (1 + I * I * I) = 2 := by
   -- conv_lhs => norm_numI
   -- conv_rhs => norm_numI
-  norm_num1
+  norm_numI
   -- rfl -- obviously this fails, demonstrating that `normalize` is necessary
   -- (a cool example if we ever want to explain how `parse` and `normalize` interact)
   -- conv_lhs => norm_numI -- Interestingly also fails
@@ -50,23 +52,23 @@ example : (1 + I) * (1 + I * I * I) = 2 := by
   conv_lhs => norm_numI
   conv_rhs => norm_numI
 
-example : (1 + 3.5 + I) * (1 + I) = 7 / 2 + 11 / 2 * I := by norm_num1
-example : (3 + 4 * I)⁻¹ * (3 + 4 * I) = 1 := by norm_num1
-example : -1 / (1 + I) = (I - 1) / 2 := by norm_num1
-example : (1 + I) * (1 - I) = 2 := by norm_num1
-example : (1 + 2 * I) - (1 + 2 * I) = 0 := by norm_num1
-example : (conj (3 + 4 * I) : ℂ) * (3 + 4 * I) = 25 := by norm_num1
-example : (3 + I : ℂ) ^ 2 = 8 + 6 * I := by norm_num1
-example : (3 : ℂ) ^ 2 + (4 : ℂ) ^ 2 = 25 := by norm_num1
+example : (1 + 3.5 + I) * (1 + I) = 7 / 2 + 11 / 2 * I := by norm_numI
+example : (3 + 4 * I)⁻¹ * (3 + 4 * I) = 1 := by norm_numI
+example : -1 / (1 + I) = (I - 1) / 2 := by norm_numI
+example : (1 + I) * (1 - I) = 2 := by norm_numI
+example : (1 + 2 * I) - (1 + 2 * I) = 0 := by norm_numI
+example : (conj (3 + 4 * I) : ℂ) * (3 + 4 * I) = 25 := by norm_numI
+example : (3 + I : ℂ) ^ 2 = 8 + 6 * I := by norm_numI
+example : (3 : ℂ) ^ 2 + (4 : ℂ) ^ 2 = 25 := by norm_numI
 
-example : 3 + I ≠ I ^ 2 := by norm_num1
-example : I ^ 2 ≠ 3 := by norm_num1
-example : 1 + I ≠ 0 := by norm_num1
-example : 1 + I ≠ 1 + 2 * I := by norm_num1
+example : 3 + I ≠ I ^ 2 := by norm_numI
+example : I ^ 2 ≠ 3 := by norm_numI
+example : 1 + I ≠ 0 := by norm_numI
+example : 1 + I ≠ 1 + 2 * I := by norm_numI
 
-example : re ((1 + 3 * I)⁻¹) = 0.1 := by norm_num1
-example : im ((1 + 3 * I)⁻¹) = -0.3 := by norm_num1
-example : 10 * re ((1 + 3 * I)⁻¹) = 1 := by norm_num1
+example : re ((1 + 3 * I)⁻¹) = 0.1 := by norm_numI
+example : im ((1 + 3 * I)⁻¹) = -0.3 := by norm_numI
+example : 10 * re ((1 + 3 * I)⁻¹) = 1 := by norm_numI
 
 example : (37 : ℕ) + 5 = 42 := rfl
 example : (37 : ℤ) + 5 = 42 := rfl
@@ -78,21 +80,21 @@ error: unsolved goals
 ⊢ False
 -/
 #guard_msgs in
-example : I^3 + I = 1 := by norm_num1
+example : I^3 + I = 1 := by norm_numI
 
 /--
 error: unsolved goals
 ⊢ False
 -/
 #guard_msgs in
-example : 1 + I = 1 + 2 * I := by norm_num1
+example : 1 + I = 1 + 2 * I := by norm_numI
 
 /--
 error: unsolved goals
 ⊢ False
 -/
 #guard_msgs in
-example : 2 * I ≠ 2 * I := by norm_num1
+example : 2 * I ≠ 2 * I := by norm_numI
 
 /--
 error: unsolved goals
