593e74d02f09a9cb130a398093a185bdb5b4be97
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2025-06-18 15:04:02 +0200

    Update upperhalfplane.lean

--- SpherePacking/ModularForms/upperhalfplane.lean
+++ SpherePacking/ModularForms/upperhalfplane.lean
@@ -20,4 +20,4 @@ lemma pos_nat_div_upper (n : ℤ) (hn : 0 < n) (z : ℍ) : 0 < (-(n : ℂ) / z).
     Left.neg_pos_iff, div_neg_iff]
   right
   rw [neg_re, neg_mul, Left.neg_neg_iff, Complex.normSq_pos, ne_eq]
-  exact ⟨by apply mul_pos (by simp [hn]) z.2; , ne_zero z⟩
+  exact ⟨by apply mul_pos (by simp [hn]) z.2, ne_zero z⟩
