95c058f82e0e56feac9ce0aedf32df775bf859b4
Rmal <97214596+CoolRmal@users.noreply.github.com>
2025-09-29 18:36:30 -0400

    Update SchwartzMap.lean

--- SpherePacking/ForMathlib/PoissonSummation/New/SchwartzMap.lean
+++ SpherePacking/ForMathlib/PoissonSummation/New/SchwartzMap.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Sidharth Hariharan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Sidharth Hariharan, Aaron Lin
+Authors: Sidharth Hariharan, Yongxi Lin
 -/
 
 import Mathlib.Analysis.Fourier.AddCircleMulti
