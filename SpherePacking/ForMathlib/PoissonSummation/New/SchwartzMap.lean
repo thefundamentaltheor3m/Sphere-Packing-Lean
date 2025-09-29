dc144692a08a33d53f4c82ab3b17650dcc1208d0
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2025-09-29 18:31:09 -0400

    add SchwartzMap file

---
+++ SpherePacking/ForMathlib/PoissonSummation/New/SchwartzMap.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2025 Sidharth Hariharan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sidharth Hariharan, Aaron Lin
+-/
+
+import Mathlib.Analysis.Fourier.AddCircleMulti
+
+/-!
+# Summability of mFourier coefficients of Schwartz Functions on ℝⁿ
+-/
