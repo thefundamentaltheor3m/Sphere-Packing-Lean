1e523a5e82b64cf5ccc8766e943b9a947a295cfb
Seewoo Lee <seewoo5@gmail.com>
2025-11-15 13:24:59 +0100

    more statements

--- SpherePacking/ModularForms/FG.lean
+++ SpherePacking/ModularForms/FG.lean
@@ -4,22 +4,38 @@ import Mathlib.Order.Monotone.Defs
 import SpherePacking.ModularForms.Derivative
 import SpherePacking.ModularForms.JacobiTheta
 
-
+open Filter
 open scoped Real
 
+
 /--
-Definition of $F$ and $G$.
+Definition of $F$ and $G$ and auxiliary functions for the inequality between them
+on the imaginary axis.
 -/
 noncomputable def F := (E₂ * E₄.toFun - E₆.toFun) ^ 2
 
 noncomputable def G := H₂ ^ 3 * (2 * H₂ ^ 2 + 5 * H₂ * H₄ + 5 * H₄ ^ 2)
 
-/--
-Prove modular linear differential equation satisfied by $F$.
--/
 noncomputable def X₄₂ := 288⁻¹ * (E₄.toFun - E₂ * E₂)
 
 noncomputable def Δ_fun := 1728⁻¹ * (E₄.toFun ^ 3 - E₆.toFun ^ 2)
+
+noncomputable def L₁₀ := (D F) * G - F * (D G)
+
+noncomputable def SerreDer_22_L₁₀ := serre_D 22 L₁₀
+
+noncomputable def FReal (t : ℝ) : ℝ := (F.resToImagAxis t).re
+
+noncomputable def GReal (t : ℝ) : ℝ := (G.resToImagAxis t).re
+
+noncomputable def FmodGReal (t : ℝ) : ℝ := (FReal t) / (GReal t)
+
+theorem F_eq_FReal (t : ℝ) (ht : 0 < t) : F.resToImagAxis t = FReal t := by sorry
+
+theorem G_eq_GReal (t : ℝ) (ht : 0 < t) : G.resToImagAxis t = GReal t := by sorry
+
+theorem FmodG_eq_FmodGReal (t : ℝ) (ht : 0 < t) :
+    FmodGReal t = (F.resToImagAxis t) / (G.resToImagAxis t) := by sorry
 
 theorem F_aux : D F = 5 * 6⁻¹ * E₂ ^ 3 * E₄.toFun ^ 2 - 5 * 2⁻¹ * E₂ ^ 2 * E₄.toFun * E₆.toFun
     + 5 * 6⁻¹ * E₂ * E₄.toFun ^ 3 + 5 * 3⁻¹ * E₂ * E₆.toFun ^ 2 - 5 * 6⁻¹ * E₄.toFun^2 * E₆.toFun
@@ -56,9 +72,35 @@ Modular linear differential equation satisfied by $G$.
 theorem MLDE_G : serre_D 12 (serre_D 10 G) = 5 * 6⁻¹ * G - 640 * Δ_fun * H₂ := by
   sorry
 
-/- $t \mapsto F(it) / G(it)$ is monotone decreasing. -/
-theorem FG_antitone : AntitoneOn (fun t : ℝ => (F.resToImagAxis t).re / (G.resToImagAxis t).re)
-    (Set.Ioi 0) := by
+theorem L₁₀_SerreDer : L₁₀ = (serre_D 10 F) * G - F * (serre_D 10 G) := by
+  sorry
+
+theorem SerreDer_22_L₁₀_SerreDer :
+    SerreDer_22_L₁₀ = (serre_D 12 (serre_D 10 F)) * G - F * (serre_D 12 (serre_D 10 G)) := by
+sorry
+
+/- $\partial_{22} \mathcal{L}_{1, 0}$ is positive on the imaginary axis. -/
+theorem SerreDer_22_L₁₀_pos : ResToImagAxis.Pos SerreDer_22_L₁₀ := by
+  sorry
+
+/- $\mathcal{L}_{1, 0}$ is eventually positive on the imaginary axis. -/
+theorem L₁₀_eventuallyPos : ResToImagAxis.EventuallyPos L₁₀ := by
+  sorry
+
+/- $\mathcal{L}_{1, 0}$ is positive on the imaginary axis. -/
+theorem L₁₀_pos : ResToImagAxis.Pos L₁₀ := antiSerreDerPos SerreDer_22_L₁₀_pos L₁₀_eventuallyPos
+
+/--
+$t \mapsto F(it) / G(it)$ is monotone decreasing.
+-/
+theorem FmodG_antitone : AntitoneOn FmodGReal (Set.Ioi 0) := by
+  sorry
+
+/--
+$\lim_{t \to 0^+} F(it) / G(it) = 18 / \pi^2$.
+-/
+theorem FmodG_rightLimitAt_zero :
+    Tendsto FmodGReal (nhdsWithin 0 (Set.Ioi 0)) (nhdsWithin (18 * (π ^ (-2 : ℤ))) Set.univ) := by
   sorry
 
 /--
