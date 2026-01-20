116c7acdcb1ca9964088a8f364d01815d5476c21
Seewoo Lee <49933279+seewoo5@users.noreply.github.com>
2026-01-20 08:15:02 -0800

    add `Hi_MDifferentiable`s (#273)

--- SpherePacking/ModularForms/JacobiTheta.lean
+++ SpherePacking/ModularForms/JacobiTheta.lean
@@ -261,6 +261,12 @@ noncomputable def H₄_SIF : SlashInvariantForm (Γ 2) 2 where
   toFun := H₄
   slash_action_eq' := slashaction_generators_Γ2 H₄ (2 : ℤ) H₄_α_action H₄_β_action H₄_negI_action
 
+@[simp] lemma H₂_SIF_coe : (H₂_SIF : ℍ → ℂ) = H₂ := rfl
+
+@[simp] lemma H₃_SIF_coe : (H₃_SIF : ℍ → ℂ) = H₃ := rfl
+
+@[simp] lemma H₄_SIF_coe : (H₄_SIF : ℍ → ℂ) = H₄ := rfl
+
 end H_SlashInvariant
 
 
@@ -345,6 +351,15 @@ lemma H₄_SIF_MDifferentiable : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) H₄_SIF :=
       have := congrArg (fun f : ℍ → ℂ => f τ) hfun_eq.symm
       simpa [Function.comp] using this)
   simpa [mdifferentiableWithinAt_univ] using hMD_within
+
+lemma H₂_MDifferentiable : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) H₂ := by
+  simpa [H₂_SIF, SlashInvariantForm.coe_mk] using H₂_SIF_MDifferentiable
+
+lemma H₃_MDifferentiable : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) H₃ := by
+  simpa [H₃_SIF, SlashInvariantForm.coe_mk] using H₃_SIF_MDifferentiable
+
+lemma H₄_MDifferentiable : MDifferentiable 𝓘(ℂ) 𝓘(ℂ) H₄ := by
+  simpa [H₄_SIF, SlashInvariantForm.coe_mk] using H₄_SIF_MDifferentiable
 
 end H_MDifferentiable
 
@@ -559,6 +574,12 @@ noncomputable def H₄_MF : ModularForm (Γ 2) 2 := {
   holo' := H₄_SIF_MDifferentiable
   bdd_at_cusps' hc := bounded_at_cusps_of_bounded_at_infty hc isBoundedAtImInfty_H₄_slash
 }
+
+@[simp] lemma H₂_MF_coe : (H₂_MF : ℍ → ℂ) = H₂ := rfl
+
+@[simp] lemma H₃_MF_coe : (H₃_MF : ℍ → ℂ) = H₃ := rfl
+
+@[simp] lemma H₄_MF_coe : (H₄_MF : ℍ → ℂ) = H₄ := rfl
 
 /-!
 ## Jacobi identity
